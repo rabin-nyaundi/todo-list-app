e7885eec1424158304784b83474da081255b54bd
rabin-nyaundi <nyaundirabin@gmail.com>
2023-05-03 14:41:45 +0300

    fix module not found error, buttons

---
+++ src/components/buttons/buttonFilled.tsx
@@ -0,0 +1,14 @@
+import { ButtonProps } from "../../../types/types";
+
+function ButtonFilled({ title, disabled }: ButtonProps) {
+  return (
+    <button
+      disabled={disabled}
+      className="px-4 py-4 bg-blue hover:bg-lightBlue hover:border-darkBlue hover:rounded-[4px] disabled:rounded-[4px] disabled:bg-darkGray text-lightGray hover:text-white font-medium text-[15px]"
+    >
+      {title}
+    </button>
+  );
+}
+
+export default ButtonFilled;
